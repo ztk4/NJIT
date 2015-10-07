03d749c992314aecb207e1e7c45bfda92e5be2c0
Zachary Kaplan <zacharytrent@gmail.com>
2015-10-07 10:37:57 -0400

    Removed Test Code

--- cs114h/prj1/SortedList.java
+++ cs114h/prj1/SortedList.java
@@ -126,40 +126,4 @@ public class SortedList<E extends Comparable<? super E>> extends List<E> {
 	}
 
 	//protected Node<E> head inherited from List
-	
-	/**
-	 * Test routine for this class.
-	 *
-	 * @param args	a <code>String[]</code> of the args passed to this program from the CLI
-	 */
-	public static void main(String[] args) {
-		Random rand = new Random(1);
-        SortedList<Integer> list = new SortedList<Integer>();
-        int m = args.length == 1 ? Integer.parseInt(args[0]) : 10;
-
-        System.out.println("insert");
-        for (int i = 0; i < m; ++i) {
-            int n = rand.nextInt(m);
-            list.insert(n);
-            System.out.print(n + ": ");
-            for (Integer j : list) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
-        }
-
-        rand = new Random(1);
-
-        System.out.println("remove");
-        for (int i = 0; i < m; ++i) {
-            int n = rand.nextInt(m);
-            list.remove(n);
-            System.out.print(n + ": ");
-            for (Integer j : list) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
-        }
-	}
-
 }
