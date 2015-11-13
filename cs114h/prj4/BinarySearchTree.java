2cd5bfd4595bf369ef684c842e977ad5af0716aa
Zachary Kaplan <zacharytrent@gmail.com>
2015-11-13 12:42:11 -0500

    Finished BinarySearchTree

--- cs114h/prj4/BinarySearchTree.java
+++ cs114h/prj4/BinarySearchTree.java
@@ -7,6 +7,8 @@
  */
 
 import java.util.Iterator;
+import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Stores comparable in a sorted Binary Tree.
@@ -17,7 +19,7 @@ import java.util.Iterator;
  * @version 	1.0
  */
 public class BinarySearchTree<E extends Comparable<? super E>> extends BinaryTree<E> {
-	
+
 	public void insert(E data) {
 		root = insert(data, root);
 	}
@@ -71,7 +73,7 @@ public class BinarySearchTree<E extends Comparable<? super E>> extends BinaryTre
 	}
 
 	public boolean search(E data) {
-		search(data, root);
+		return search(data, root);
 	}
 
 	private boolean search(E data, Node<E> root) {
@@ -89,9 +91,97 @@ public class BinarySearchTree<E extends Comparable<? super E>> extends BinaryTre
 	}
 
 	public Iterator<E> iterator() {
-		return new Iterator<>() {
+		return new Iterator<E>() {
 			
+			Object[] stack = new Object[20];
+			int end, leftDone = -1;
+
+			{
+				if(root == null)
+					end = -1;
+				else
+					stack[end] = root;
+			}
+
+			private void setCapacity(int capacity) {
+				Object[] tmp = new Object[capacity];
+
+				for(int i = 0; i <= end; ++i)
+					tmp[i] = stack[i];
+
+				stack = tmp;
+			}
+
+			public boolean hasNext() {
+				return end >= 0;	
+			}
+
+			@SuppressWarnings("unchecked")
+			public E next() {
+				if(leftDone < end) {
+					while( ((Node<E>) stack[end]).left != null) {
+						if(end == stack.length - 1)
+							setCapacity(stack.length * 2);
+
+						stack[end + 1] = ((Node<E>) stack[end]).left; 		//push node to stack
+						++end;
+					}
+				}
+
+				Node<E> tmp = (Node<E>) stack[end--]; 					//pop node off stack
+				leftDone = end;
+
+				if(tmp.right != null)
+					stack[++end] = tmp.right;							//push right node to stack
+				else if(end < stack.length / 3 && stack.length >= 10) 	//only popped, no push, might need to shrink
+					setCapacity(stack.length / 2);
+
+				return tmp.data;
+			}
+
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+
 		};
 	}
 
+	public static void main(String[] args) {
+		int N = args.length < 1 ? 10 : Integer.parseInt(args[0]);
+		Random rand = new Random(1);
+
+		int[] arr = new int[N];
+		BinaryTree<Integer> bst = new BinarySearchTree<>();
+
+		for(int i = 0; i < N; ++i) {
+			arr[i] = rand.nextInt(N);
+			bst.insert(arr[i]);
+		}
+
+		Arrays.sort(arr);
+		Iterator<Integer> it = bst.iterator();
+
+		System.out.println(Arrays.toString(arr));
+
+		System.out.print('[');
+		for(int i : bst) {
+			System.out.print(i + ", ");
+		}
+		System.out.println("\b\b]");
+
+		for(int i : arr) {
+			if(!it.hasNext()) {
+				System.err.println("No Next Element");
+				System.exit(1);
+			}
+
+			if(i != it.next()) {
+				System.err.println("Element Mismatch");
+				System.exit(1);
+			}
+		}
+
+		System.out.println("All Good!");
+	}
+	
 }
