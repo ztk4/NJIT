c86c4531dc068ca771d9438c553c2f5e4e776f11
Zachary Kaplan <zacharytrent@gmail.com>
2016-02-16 12:26:15 -0500

    Add2 updated and Makefile changed to gen output

--- cs252/hw/add2/add2.s
+++ cs252/hw/add2/add2.s
@@ -3,6 +3,5 @@
 add2:   STMFD   sp!, {v1-v6, lr}    @ add2 routine to take two integers and return the sum
         ADD     a1, a1, a2          @ sum a1 and a2 into a1
         LDMFD   sp!, {v1-v6, pc}
-
         .end
 
