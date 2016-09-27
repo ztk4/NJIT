ed21fcc2f0c722bf42d6aef425be83c12e02d338
Zachary Kaplan <zacharytrent@gmail.com>
2016-09-27 16:48:34 -0400

    CS288 HW03
    Variable Length Columns!

--- cs288/hw03/table.sh
+++ cs288/hw03/table.sh
@@ -38,17 +38,15 @@ function get_title {
 # Print Multiple
 # pm str n
 function pm {
+  local i=
   for (( i = 0; i < $2; ++i )); do echo -n "$1"; done
 }
 
 # Print header
 # ph str len
 function ph {
-  oLang="$LANG" 
-  LANG='en_US.utf8' # For getting displayed string length
   (( buf1 = ($2 - ${#1}) / 2 ))
   (( buf2 = ($2 - ${#1} + 1) / 2 ))  # In case odd
-  LANG="$oLang"
   pm " " $buf1
   echo -n "$1"
   pm " " $buf2
@@ -58,10 +56,7 @@ function ph {
 # Print cell left
 # pcl str len
 function pcl {
-  oLang="$LANG" 
-  LANG='en_US.utf8' # For getting displayed string length
   (( trail = $2 - ${#1} - 1 ))
-  LANG="$oLang"
   echo -n " $1"
   pm " " $trail
   echo -n "|"
@@ -70,10 +65,7 @@ function pcl {
 # Print cell right
 # pcr str len
 function pcr {
-  oLang="$LANG" 
-  LANG='en_US.utf8' # For getting displayed string length
   (( head = $2 - ${#1} - 1 ))
-  LANG="$oLang"
   pm " " $head
   echo -n "$1 "
   echo -n "|"
@@ -82,26 +74,70 @@ function pcr {
 
 # main [file=index.html]
 function main {
+  local oLang=$LANG
+  LANG='en_US.utf8'  # For getting printed string length
+
   if [ -z "$1" ]; then
     local file=index.html
   else
     local file="$1"
   fi
-  # Column widths are hardcoded
-  # Not worth the effort to make it adjustable, pretty enough as is!
-  local bar_len=180
+  
+  local users=()
+  local views=()
+  local durations=()
+  local ids=()
+  local title=()
+
+  local maxlen_user=0
+  local maxlen_views=0
+  local maxlen_duration=0
+  local maxlen_id=0
+  local maxlen_title=0
+
+  # All lines we care about should contain xxx,xxx views
+  IFS=$'\n' lines=($(egrep "[0-9,]+ views" "$file"))
+  local line=
+  for line in ${lines[@]}; do
+    # Add onto storage arrays
+    users+=("$(get_user "$line")")
+    views+=("$(get_views "$line")")
+    durations+=("$(get_duration "$line")")
+    ids+=("$(get_id "$line")")
+    titles+=("$(get_title "$line")")
+
+    # Set user to N/A if video not posted by a user
+    [ -z "${users[-1]}" ] && users[-1]="N/A"
+
+    # Set maxlen if last elt added is longer than current max
+    [ ${#users[-1]} -gt $maxlen_user ] && local maxlen_user=${#users[-1]}
+    [ ${#views[-1]} -gt $maxlen_views ] && local maxlen_views=${#views[-1]}
+    [ ${#durations[-1]} -gt $maxlen_duration ] && local maxlen_duration=${#durations[-1]}
+    [ ${#ids[-1]} -gt $maxlen_id ] && local maxlen_id=${#ids[-1]}
+    [ ${#titles[-1]} -gt $maxlen_title ] && local maxlen_title=${#titles[-1]}
+  done
+
+  # Account for title boxes
+  [ 4 -gt $maxlen_user ] && local maxlen_user=4           # len(User)     = 4
+  [ 5 -gt $maxlen_views ] && local maxlen_views=5         # len(Views)    = 5
+  [ 8 -gt $maxlen_duration ] && local maxlen_duration=8   # len(Duration) = 8
+  [ 3 -gt $maxlen_id ] && local maxlen_id=3               # len(VID)      = 3
+  [ 5 -gt $maxlen_title ] && local maxlen_title=5         # len(Title)    = 5
+
+  # Account for 2 space padding
+  local user_len=$(( maxlen_user + 2 ))
+  local views_len=$(( maxlen_views + 2 ))
+  local duration_len=$(( maxlen_duration + 2 ))
+  local id_len=$(( maxlen_id + 2 ))
+  local title_len=$(( maxlen_title + 2 ))
+
+  # Lengths  + (n - 1 internal bars)
+  local bar_len=$(( user_len + views_len + duration_len + id_len + title_len + 4 ))
 
   echo -n +
   pm - $bar_len
   echo +
   
-  local user_len=22
-  local views_len=13
-  local duration_len=10
-  local id_len=13
-  # The 4 accounts for the bars between cells
-  (( title_len = bar_len - user_len - views_len - duration_len - id_len - 4 ))
-
   echo -n "|"
   ph User $user_len
   ph Views $views_len
@@ -114,23 +150,14 @@ function main {
   pm - $bar_len
   echo "|"
 
-  # All lines we care about should contain xxx,xxx views
-  IFS=$'\n' lines=($(egrep "[0-9,]+ views" "$file"))
-  for line in ${lines[@]}; do
-    local user="$(get_user "$line")"
-    local views="$(get_views "$line")"
-    local duration="$(get_duration "$line")"
-    local id="$(get_id "$line")"
-    local title="$(get_title "$line")"
-
-    [ -z "$user" ] && local user="N/A"
-
+  local i=
+  for (( i = 0; i < ${#users[@]}; ++i )); do
     echo -n "|"
-    pcl "$user" $user_len
-    pcr "$views" $views_len
-    pcr "$duration" $duration_len
-    pcl "$id" $id_len
-    pcl "$title" $title_len
+    pcl "${users[$i]}" $user_len
+    pcr "${views[$i]}" $views_len
+    pcr "${durations[$i]}" $duration_len
+    pcl "${ids[$i]}" $id_len
+    pcl "${titles[$i]}" $title_len
     echo
   done
 
@@ -138,7 +165,9 @@ function main {
   pm - $bar_len
   echo +
 
-  echo "Printed ${#lines[@]} rows."
+  echo "Printed ${#users[@]} rows."
+
+  LANG=$oLang
 }
 
 [ "$?" -le 1 ] && { main "$1"; true; } || echo "Usage: $0 [file=index.html]"
