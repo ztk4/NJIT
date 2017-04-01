edf11a55bc2abcc789bebd21e1c0938a3c407671
Zachary Kaplan <zacharytrent@gmail.com>
2017-03-31 21:09:17 -0400

    Added a DistanceVectorTable class
    Manages a router's distance table, and can update it based on a neighboring router's table

---
+++ cs356/p2/src/distance_vector_table.cpp
@@ -0,0 +1,64 @@
+#include "distance_vector_table.h"
+
+#include <iomanip>
+
+using namespace std;
+
+namespace router {
+DistanceVectorTable::DistanceVectorTable(uint16_t source_router_id) {
+  table_[source_router_id] = 0;
+}
+
+bool DistanceVectorTable::AddEdge(uint16_t router_id, int16_t distance) {
+  auto res = table_.emplace(router_id, distance);
+  return res.second;
+}
+
+const map<uint16_t, int16_t> &DistanceVectorTable::Table() const {
+  return table_;
+}
+
+bool DistanceVectorTable::UpdateTable(uint16_t source_id,
+    const map<uint16_t, int16_t> &table) {
+  // Lookup source_id.
+  auto it = table_.find(source_id);
+  if (it == table_.end())
+    return false;
+
+  // Distance to source_id.
+  int16_t prepend = it->second;
+  // Whether or not the internal table was updated.
+  bool edited = false;
+  
+  for (const auto &entry : table) {
+    int16_t distance = prepend + entry.second;
+    // First try inserting the new edge (path doesn't already exist).
+    auto res = table_.emplace(entry.first, distance);
+    if (res.second) {  // Successful insert.
+      edited = true;
+    } else {  // Edge already existed.
+      // Compare the calculated distance to the current value, update if needed.
+      if (distance < res.first->second) {
+        res.first->second = distance;
+        edited = true;
+      }
+    }
+  }
+
+  return edited;
+}
+}  // namespace router
+
+ostream &operator<<(ostream &o, const router::DistanceVectorTable &dvt) {
+  const char *kBreak = "+-----------------+";
+  o << kBreak << endl;
+  o << "|   ROUTING TABLE   |" << endl;
+  o << kBreak << endl;
+
+  for (const auto &entry : dvt.Table()) {
+    o << "| " << setw(6) << entry.first
+      << " | " << setw(6) << entry.second << " |" << endl;
+  }
+
+  return o << kBreak << endl;
+}
---
+++ cs356/p2/src/distance_vector_table.h
@@ -0,0 +1,50 @@
+#ifndef ROUTER_DISTANCE_VECTOR_TABLE_H_
+#define ROUTER_DISTANCE_VECTOR_TABLE_H_
+
+#include <cstdint>
+#include <iostream>
+#include <map>
+
+namespace router {
+class DistanceVectorTable {
+ public:
+  /// Constructs a new table with the specified router as the source.
+  ///
+  /// @param source_router_id the router id of this router. Will insert a 
+  ///                         self referencial edge with weight 0.
+  explicit DistanceVectorTable(uint16_t source_router_id);
+  // Disallow Copy & Assign
+  DistanceVectorTable(const DistanceVectorTable &) = delete;
+  DistanceVectorTable &operator=(const DistanceVectorTable &) = delete;
+
+  /// Adds an edge to this table to the specified router with the given
+  /// distance if an edge to that router does not already exist.
+  ///
+  /// @param router_id the id of the router to add an edge to.
+  /// @param distance the weight of the edge.
+  ///
+  /// @returns true if the edge was added, and false if the edge already exists.
+  bool AddEdge(uint16_t router_id, int16_t distance);
+
+  /// @returns a const reference to the mapping table.
+  const std::map<uint16_t, int16_t> &Table() const;
+
+  /// Updates this table with new edges from a specified known source.
+  ///
+  /// @param source_id the router id of the source of this new table.
+  /// @param table that routers table.
+  ///
+  /// @returns true if any update was made to this table, false otherwise.
+  bool UpdateTable(uint16_t source_id,
+      const std::map<uint16_t, int16_t> &table);
+
+ private:
+  std::map<uint16_t, int16_t> table_;
+};
+}  // namespace router
+
+/// Printing operator overload for DistanceVectorTable.
+std::ostream &operator<<(std::ostream &o,
+    const router::DistanceVectorTable &dvt);
+
+#endif  // ROUTER_DISTANCE_VECTOR_TABLE_H_
