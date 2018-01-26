d1f98b55db06814911e3fafd7f764f7a4325f92c
Zach <zacharytrent@gmail.com>
2018-01-25 23:28:02 -0500

    Cleanup

--- cs680/hw02/test_modules.sh
+++ cs680/hw02/test_modules.sh
@@ -8,6 +8,8 @@ for module in *.ko; do
 done
 
 # Now try the third module with custom args
-insmod hello3.ko recipient='"Zachary Kaplan"' addr_lines='"141 Warren Street","Room #329A"' city="Newark" zip=7103
+insmod hello3.ko recipient='"Zachary Kaplan"' addr_lines='"141 Warren Street,Room #329A"' city="Newark" zip=7103
 sleep 1
 rmmod hello3.ko
+
+dmesg
