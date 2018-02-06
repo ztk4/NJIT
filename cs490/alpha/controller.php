6d74eca38492bb2abef76a562cf811373ea93830
Zachary Kaplan <zacharytrent@gmail.com>
2018-02-06 13:33:38 -0500

    Local logic finished

--- cs490/alpha/controller.php
+++ cs490/alpha/controller.php
@@ -23,6 +23,8 @@
 // Constants used within this script.
 $njit_webauth = "https://cp4.njit.edu/cp/home/login";
 $njit_key = 'njitSuccess';
+$local_db = "https://web.njit.edu/~UCID/path/to/script.php";  // TODO: Fill in.
+$local_key = 'localSuccess';
 
 // Set the content type for good measure.
 header('Content-Type: application/json');
@@ -57,17 +59,32 @@ if (!isset($_POST['user'])) {
   if ($code == 200) {
     // Code 200 means that the response was OK and we were returned an 'Invalid
     // Login' page.
-    $data['njitSuccess'] = false;
+    $data[$njit_key] = false;
   } else if ($code == 302) {
     // Code 302 means that the login was successful, and the client should
     // move on to the Location header to continue the login process.
-    $data['njitSuccess'] = true;
+    $data[$njit_key] = true;
   } else {
     $data['err'] = "Failed to connect to the NJIT login page.";
   }
 
   // Second, lets hit the backend storage script.
-  // TODO: this.
+  $local_post = array(
+    'user' => $user,  // TODO: Ensure this format is okay.
+    'pass' => $pass
+  );
+  $cp = curl_init($local_db);
+  $local_post = http_build_query($local_post);
+  curl_setopt($cp, CURLOPT_POST, 1);
+  curl_setopt($cp, CURLOPT_POSTFIELDS, $local_post);
+  curl_setopt($cp, CURLOPT_RETURNTRANSFER, true);
+
+  $resp = json_decode(curl_exec($cp));
+  if (is_null($resp)) {
+    $data['err'] = "Failed to parse local db response as JSON";
+  } else {
+    $data[$local_key] = $resp->found;
+  }
 }
 
 // Set code to Bad Request if an error occurred.
